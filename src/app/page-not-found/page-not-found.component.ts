90c86c3903a814d2bde0e805489ea3077857700f
vincent <v19961112@gmail.com>
2020-05-22 00:56:04 +0800

    login page swim product introduction

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -5,15 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-// const Height404 = document.getElementById('Height404');
-// // tslint:disable-next-line: only-arrow-functions
-// window.onmousemove = function(e: { clientX: any; clientY: any; }) {
-//   // tslint:disable-next-line: one-variable-per-declaration
-//   const x = e.clientX,
-//       y = e.clientY;
-//   Height404.style.backgroundPositionX = x + 'px';
-//   Height404.style.backgroundPositionY = y + 'px';
-// };
+
 export class PageNotFoundComponent implements OnInit {
 
   constructor() { }
--- src/app/swim-introduction/swim-introduction.component.ts
+++ src/app/swim-introduction/swim-introduction.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwimIntroductionComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
